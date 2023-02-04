b49b4ae38b3232a0aacfceb1e78e3f5863e08ebb
bansaldh <bansal.d@northeastern.edu>
2023-02-04 13:09:29 -0500

    LAB1

---
+++ LAB1/build/catkin_generated/installspace/gps_driver-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(gps_driver_MESSAGE_FILES "gps_driver/msg/gps_msg.msg")
+set(gps_driver_SERVICE_FILES "")
---
+++ LAB1/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ubuntu20_04/EECE5554/LAB1/devel/include".split(';') if "/home/ubuntu20_04/EECE5554/LAB1/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime;roscpp;rospy;sensor_msgs;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lgps_driver".split(';') if "-lgps_driver" != "" else []
+PROJECT_NAME = "gps_driver"
+PROJECT_SPACE_DIR = "/home/ubuntu20_04/EECE5554/LAB1/devel"
+PROJECT_VERSION = "0.0.0"
